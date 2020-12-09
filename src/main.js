19ba64c4b6fdb7a75205738b374a8bcc4f3be614
CharlyLuna <cluna10@ucol.mx>
2020-12-09 11:32:14 -0600

    Implemented sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -7,8 +7,25 @@ export default class App {
         }
         return suma;
     }
+
+    sumatoriaSerieDos(numero) {
+        let suma = 1;
+        let i = 2;
+        let impares = 0;
+        while(i <= numero) {
+            if( i % 2 === 0) {
+                suma = suma + 1 / i;
+            } else {
+                impares = impares + 1 / i;
+            }
+            i++; 
+        }   
+        return suma - impares;
+    }
 }
 
 let app = new App();
 
 console.log(app.sumatoriaSerieUno(7));
+
+console.log(app.sumatoriaSerieDos(7));
