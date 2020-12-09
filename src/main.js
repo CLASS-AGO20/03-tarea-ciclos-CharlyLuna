36c186918c5124bd945e779bd1a453716c1e94a8
CharlyLuna <cluna10@ucol.mx>
2020-12-09 10:53:40 -0600

    Implemented sumatoriaSerieUno

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero) {
+        let suma = 0;
+        for(let i = 1; i <= numero; i = i + 1 ) {
+            suma = suma + 1 / i;
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(7));
